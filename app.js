be9a4c4c417dfa0474dcfcc400b668caa40b5ecf
Amit Ambardekar <amitamb@gmail.com>
2016-05-10 05:16:41 +0530

    Defined start script

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.use(express.static('public'));
 //   res.send('Hello World!');
 // });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
