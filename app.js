8d763dd62ae750dff147e2c0387f10fa93380eaa
Amit Ambardekar <amitamb@gmail.com>
2016-05-10 06:54:37 +0530

    Basic functionality working

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
 
@@ -7,7 +9,15 @@ app.use(express.static('public'));
 //   res.send('Hello World!');
 // });
 
-var port = process.env.PORT || 3000;
+var redis = require('redis');
+if (process.env.REDISCLOUD_URL) {
+  var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+}
+else {
+  var client = redis.createClient({no_ready_check: true});
+}
+
+var port = process.env.PORT;
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
